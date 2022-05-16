254de67c743362a2365357952a37c1635e7defd7
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-05-16 11:50:10 +0100

    Added file upload service

---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Observable, Subject } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+//import { FileUpload } from '../models/fileUpload';
+import { FireAuthService } from './firestore/fire-auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private basePath = '/users';
+  constructor(private storage: AngularFireStorage, private fireAuth: FireAuthService) { }
+
+  /*pushFileToStorage(fileUpload: FileUpload): void {
+    const filePath = `${this.basePath}/${fileUpload.file.name}`;
+    const storageRef = this.storage.ref(filePath);
+    const uploadTask = this.storage.upload(filePath, fileUpload.file);
+
+    uploadTask.snapshotChanges().pipe(
+
+      finalize(() => {
+        storageRef.getDownloadURL().subscribe(downloadURL => {
+
+          this.fireAuth.getCurrentUser().subscribe(user => {
+            user.updateProfile({
+              photoURL: downloadURL,
+            })
+          })
+
+        });
+      })
+    ).subscribe();
+  }*/
+}
