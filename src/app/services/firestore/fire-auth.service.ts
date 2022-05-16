bfafe3c452988beb67097976eeba3ca1d9629e99
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-05-16 11:46:23 +0100

    Added firestore services

---
+++ src/app/services/firestore/fire-auth.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FireAuthService {
+  userData: Observable<any>;
+
+  constructor(private angularFireAuth: AngularFireAuth, private router: Router) {
+  }
+
+  async singUp(email: string, password: string) {
+
+    this.angularFireAuth.createUserWithEmailAndPassword(email, password).then(resp => {
+    })
+      .catch(error => {
+        console.log("Something went wrong in register: ", error.message);
+
+      })
+
+  }
+
+  async singIn(email: string, password: string): Promise<boolean> {
+    let succesfully !: boolean;
+    await this.angularFireAuth.signInWithEmailAndPassword(email, password).then(() => {
+      succesfully = true;
+    })
+      .catch((error) => {
+        console.log("Something went wrong in login: ", error.message);
+        succesfully = false;
+      })
+    return succesfully;
+  }
+
+  SignOut() {
+    this.angularFireAuth.signOut().then(() => {
+      this.router.navigate(['/home']);
+    });
+  }
+
+  async checkEmailExists(email: string): Promise<boolean> {
+    let exists !: boolean;
+    await this.angularFireAuth.fetchSignInMethodsForEmail(email).then(user => {
+      if (user.length > 0) {
+        exists = true;
+      } else {
+        exists = false;
+      }
+    })
+    return exists;
+  }
+
+  async checkLoggedIn(): Promise<boolean> {
+    let logged !: boolean;
+    this.angularFireAuth.authState.subscribe(userResponse => {
+      if (userResponse) {
+        console.log('here is your user data');
+        localStorage.setItem('user', JSON.stringify(userResponse));
+
+        console.log(userResponse);
+        logged = true;
+      } else {
+        localStorage.setItem('user', "false");
+        logged = false;
+      }
+    });
+    return logged;
+  }
+
+  getCurrentUser(): Observable<any> {
+    var user = new Subject<any>();
+    this.angularFireAuth.authState.subscribe(userResponse => {
+      user.next(userResponse);
+    })
+    return user.asObservable();
+  }
+
+}
---
+++ src/app/services/firestore/firestore.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+//import { Route } from 'src/app/route-page/route-page.component';
+import {
+  query,
+  collection,
+  where,
+  documentId,
+  getDocs
+} from "firebase/firestore";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FirestoreService {
+
+
+
+  constructor(private firestore: AngularFirestore) { }
+
+
+  public createRoute(data: {
+    routeName: string,
+    activites: { activityId: number },
+    dateAvailability: { date: string },
+    numberOfPeople: number,
+    price: number,
+    publishDate: string,
+    startTime: string
+  }) {
+    return this.firestore.collection('predefinedRoutes').add(data);
+  }
+
+  public createContactForm(form: {
+    email: string,
+    firstName: string,
+    lastName: string,
+    messageText: string,
+    phoneNumber: number | null,
+  }) {
+    return this.firestore.collection('contactForms').add(form);
+  }
+
+
+  public getRoute(documentId: string) {
+    return this.firestore.collection('predefinedRoutes').doc(documentId).snapshotChanges();
+  }
+
+  public getActivitiesById(ids: []) {
+    return this.firestore.collection('routeActivities', ref => ref.where(documentId(), "in", ids)).valueChanges();
+  }
+
+  public getActivity(documentId: string) {
+    return this.firestore.collection('routeActivities').doc(documentId).snapshotChanges();
+  }
+
+  public getRoutes() {
+    return this.firestore.collection('predefinedRoutes').snapshotChanges();
+
+  }
+}
