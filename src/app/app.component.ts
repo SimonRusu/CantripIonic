ffc86b2e4bc94b411ec3f815c87efa93601fca26
María Naranjo Almeida <maria2000naranjo@gmail.com>
2022-05-17 21:53:10 +0100

    Customized headeer and login header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FireAuthService } from './services/firestore/fire-auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  logged !: boolean;
+  constructor(private fireAuth: FireAuthService) { }
+  ngOnInit(): void {
+    this.logged = this.checkLoggedIn();
+  }
+  checkLoggedIn(): boolean {
+    this.fireAuth.checkLoggedIn();
+    let dato = localStorage.getItem('user');
+    if (dato !== "false") {
+      console.log("logueado");
+      return true;
+    } else {
+      console.log("no logueado");
+      return false;
+    }
+  }
 }
